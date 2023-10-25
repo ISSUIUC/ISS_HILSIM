77eb152861039ea30df11a80660db9f528abdf5c
carsonsprague <cs104@illinois.edu>
2023-10-24 19:42:57 -0500

    Added lots of components for Queue

---
+++ Central-Server/hilsim-web/src/components/queuelist.js
@@ -0,0 +1,18 @@
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import QueueItem from './queueitem';
+import { Container } from 'react-bootstrap';
+
+function QueueList() {
+  return (
+    <Container>
+      <QueueItem />
+      <QueueItem />
+      <QueueItem />
+      <QueueItem />
+      <QueueItem />
+    </Container>
+  );
+}
+
+export default QueueList;
