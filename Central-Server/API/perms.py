50ccdff1403051918fd68080c7ab4fbbe92f03e4
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-10-24 19:49:31 -0500

    WOOOO

--- Central-Server/API/perms.py
+++ Central-Server/API/perms.py
@@ -9,7 +9,7 @@ def get_team_membership():
     # TOKEN = request.cookies.get('token')
     url = "https://api.github.com/orgs/ISSUIUC/teams/iss-kamaji-administrators/memberships/mpkarpov-ui"
     x = requests.get(url=url,
-                     headers={"Accept": "application/vnd.github+json", 'Authorization':"Bearer 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", "Content-Type": "text/html; charset=utf-8", "X-GitHub-Api-Version":"2022-11-28"})
+                     headers={"Accept": "application/vnd.github+json", 'Authorization':"Bearer <ACCESS CODE>", "Content-Type": "text/html; charset=utf-8", "X-GitHub-Api-Version":"2022-11-28"})
     
     print(x)
     print(x.content)
