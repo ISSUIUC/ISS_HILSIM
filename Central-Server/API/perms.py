323cc3e9a1d903c0a5ad92d7713852302511f22d
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-10-31 18:13:18 -0500

    input username

--- Central-Server/API/perms.py
+++ Central-Server/API/perms.py
@@ -4,11 +4,11 @@ import requests
 
 perms_blueprint = Blueprint('perms', __name__)
 
-@perms_blueprint.route("/perms", methods=["GET"])
-def get_team_membership():
+@perms_blueprint.route("/perms/<string:username>", methods=["GET"])
+def get_team_membership(username):
     TOKEN = request.cookies.get('token')
     # TOKEN = ''
-    url = "https://api.github.com/orgs/ISSUIUC/teams/iss-kamaji-administrators/memberships/mpkarpov-ui"
+    url = f"https://api.github.com/orgs/ISSUIUC/teams/iss-kamaji-administrators/memberships/{username}"
     x = requests.get(url=url,
                      headers={"Accept": "application/vnd.github+json", 'Authorization':f"Bearer {TOKEN}", "Content-Type": "text/html; charset=utf-8", "X-GitHub-Api-Version":"2022-11-28"})
     
