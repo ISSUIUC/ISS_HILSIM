c6ef1fbcc8ef86201845011b0cad08d19bde74c9
carsonsprague <cs104@illinois.edu>
2023-11-07 19:13:47 -0600

    forgot to add these. check prev commit

---
+++ Central-Server/hilsim-web/src/pages/newjob.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/common/navbar";
+import NewJob from "../components/newjob";
+
+function New_Job() {
+    return (
+        <div>
+            <NavBar />
+            <NewJob />
+        </div>
+    );
+} 
+
+export default New_Job;
---
+++ Central-Server/hilsim-web/src/pages/queue.js
@@ -0,0 +1,33 @@
+import { Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import QueueList from '../components/queuelist';
+import QueueItem from '../components/queueitem';
+import Status from '../components/status';
+import NewJob from '../components/newjob';
+import NavBar from '../components/common/navbar';
+
+function Queue() {
+  return (
+    <div>
+      <Container fluid>
+        <h1 style={{textAlign: 'left', fontWeight: 'initial', marginBottom: '10px'}}>
+          Queue
+        </h1>
+        <Row>
+          <Col md={3}>
+          <Status />
+          </Col>
+          <Col md={9}>
+          <QueueList />
+          </Col>
+        </Row>
+    </Container>
+    </div>
+    
+  );
+}
+
+export default Queue;
