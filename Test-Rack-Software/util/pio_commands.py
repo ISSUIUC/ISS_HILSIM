0995930dceb8dc95c8ac08de281c15e6614f939e
mpkarpov-ui <mkarpov2@illinois.edu>
2023-12-09 14:19:33 -0600

    back to single core compilation

--- Test-Rack-Software/util/pio_commands.py
+++ Test-Rack-Software/util/pio_commands.py
@@ -9,7 +9,7 @@ import os
 import sys
 import threading
 
-COMPILATION_CORE_USAGE = 2
+COMPILATION_CORE_USAGE = 1
 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
