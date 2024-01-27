9842bb8919e16f332644a9efd16be79e21f87924
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2024-01-27 17:14:45 -0600

    web server pull link from git

--- Central-Server/hilsim-web/src/dev_config.js
+++ Central-Server/hilsim-web/src/dev_config.js
@@ -1,2 +1,6 @@
-export const api_url = "http://localhost"
+const server_url_request = await fetch("https://raw.githubusercontent.com/ISSUIUC/ISS_HILSIM/active_server_url/server_url.txt");
+
+console.log(server_url_request)
+
+export const api_url = server_url_request
 // /api/jobs/list
