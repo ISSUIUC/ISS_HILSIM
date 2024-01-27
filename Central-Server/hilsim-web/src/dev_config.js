2acd96300cf007448220dd7a88a47033d80154a5
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2024-01-27 17:19:24 -0600

    actual asunc stuff working

--- Central-Server/hilsim-web/src/dev_config.js
+++ Central-Server/hilsim-web/src/dev_config.js
@@ -1,6 +1,8 @@
 const server_url_request = await fetch("https://raw.githubusercontent.com/ISSUIUC/ISS_HILSIM/active_server_url/server_url.txt");
 
-console.log(server_url_request)
+const text = await server_url_request.text();
 
-export const api_url = server_url_request
+console.log(text);
+
+export const api_url = text;
 // /api/jobs/list
