f91046b3a22d0523fbf44ddfefbe09165d24d2bd
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2024-01-27 17:35:29 -0600

    use two cores

--- Test-Rack-Software/util/pio_commands.py
+++ Test-Rack-Software/util/pio_commands.py
@@ -10,7 +10,7 @@ import sys
 import threading
 import time
 
-COMPILATION_CORE_USAGE = 1
+COMPILATION_CORE_USAGE = 2
 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
