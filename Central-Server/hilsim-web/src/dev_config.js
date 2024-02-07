8663ff4b72dbc903ea6fb191db53db2339a8c9ee
mpkarpov-ui <mkarpov2@illinois.edu>
2024-02-07 09:43:20 -0600

    Comments for frontend dev config

--- Central-Server/hilsim-web/src/dev_config.js
+++ Central-Server/hilsim-web/src/dev_config.js
@@ -1,8 +1,13 @@
-const server_url_request = await fetch("https://raw.githubusercontent.com/ISSUIUC/ISS_HILSIM/active_server_url/server_url.txt");
+/**
+ * Due to the nature of this project, we are interested in performing tests on seperate computers on the same network. Because of this, we
+ * spin up an http tunnel through Ngrok. Because the URL of the tunnel is random every time, we need to be able to update the target URL
+ * on both the frontend and the datastreamer. The datastreamer handles this through its own `dynamic_url.py` service, this (the frontend)
+ * handles dynamic URLs here.
+ * 
+ * To edit the dynamic url, edit `server_url.txt` on the active_server_url branch (https://github.com/ISSUIUC/ISS_HILSIM/tree/active_server_url)
+*/
 
+const server_url_request = await fetch("https://raw.githubusercontent.com/ISSUIUC/ISS_HILSIM/active_server_url/server_url.txt");
 const text = await server_url_request.text();
 
-console.log(text);
-
 export const api_url = text;
-// /api/jobs/list
