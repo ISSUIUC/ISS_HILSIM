2008f028b0cb50a49a7b73db11ea96bd8c5cd092
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2024-02-08 22:12:59 -0600

    fix changes

--- Test-Rack-Software/dummy_rack/interface.py
+++ Test-Rack-Software/dummy_rack/interface.py
@@ -33,6 +33,7 @@ class DummyAvionics(AVInterface.AvionicsInterface):
 
     def power_cycle(self) -> bool:
         time.sleep(1) # Emulate a synchronous power cycle
+        return True
 
     def code_pull(self, git_target: str) -> None:
         time.sleep(1) # Emulate a synchronous code pull
