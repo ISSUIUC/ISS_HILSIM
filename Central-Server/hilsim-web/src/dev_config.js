4ba281da09627c7387ac0cdadfea2780c4aebc39
Mannan Arshad <mannanarshad@wirelessprv-10-194-128-192.near.illinois.edu>
2024-10-15 20:24:18 -0500

    Fixed localhosturl in dev_config.js to clear dashboard

--- Central-Server/hilsim-web/src/dev_config.js
+++ Central-Server/hilsim-web/src/dev_config.js
@@ -3,24 +3,27 @@
  * spin up an http tunnel through Ngrok. Because the URL of the tunnel is random every time, we need to be able to update the target URL
  * on both the frontend and the datastreamer. The datastreamer handles this through its own `dynamic_url.py` service, this (the frontend)
  * handles dynamic URLs here.
- * 
+ *
  * To edit the dynamic url, edit `server_url.txt` on the active_server_url branch (https://github.com/ISSUIUC/ISS_HILSIM/tree/active_server_url)
-*/
+ */
 
-const server_url_request = await fetch("https://raw.githubusercontent.com/ISSUIUC/ISS_HILSIM/active_server_url/server_url.txt");
-let text = await server_url_request.text()
-text = text.trim()
+const server_url_request = await fetch(
+  "https://raw.githubusercontent.com/ISSUIUC/ISS_HILSIM/active_server_url/server_url.txt"
+);
+let text = await server_url_request.text();
+text = text.trim();
 
-await fetch(text).then(response => {
-    console.log("Fetched URL is valid.")
-}).catch(error => {
-    console.log("Fetched URL was not valid.")
-    console.log("Valid URL check failed with error: " + error.message)
-    console.log("Setting API URL to localhost")
+await fetch(text)
+  .then((response) => {
+    console.log("Fetched URL is valid.");
+  })
+  .catch((error) => {
+    console.log("Fetched URL was not valid.");
+    console.log("Valid URL check failed with error: " + error.message);
+    console.log("Setting API URL to localhost");
 
-    text = "http://localhost"
-});
-
+    text = "http://localhost/";
+  });
 
 // test url
 // text = "http://localhost"
